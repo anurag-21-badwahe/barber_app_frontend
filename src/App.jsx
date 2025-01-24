ac8e57013e1b806950df19b5f3826130bff74897
anurag <anuragbadwahe@gmail.com>
2025-01-24 17:24:27 +0530

    setup basic redux-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  }
+  const handleDecrement = () => {
+    dispatch(decrement());
+  }
+  const handleReset = () => {
+    dispatch(reset());
+  }
 
   return (
-  <>
-  <div className='bg-red-500 text-white p-4'>
-    hello world
-  </div>
-  </>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4 bg-red">Counter App</h1>
+      <div className="text-4xl font-semibold text-blue-500 mb-6">{count}</div>
+      <div className="flex space-x-4">
+        <button
+          onClick={handleIncrement}
+          className="px-4 py-2 bg-green-500  text-black  rounded-lg hover:bg-green-600"
+        >
+          Increment
+        </button>
+        <button
+          onClick={handleDecrement}
+          className="px-4 py-2 bg-red-500  text-black  rounded-lg hover:bg-red-600"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={handleReset}
+          className="px-4 py-2 bg-gray-500 text-black rounded-lg hover:bg-gray-600"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
